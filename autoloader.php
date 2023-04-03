940e216ea74a0df27f76c1f70606f16f0b901107
bugsinx <bugsinx@gmail.com>
2023-04-03 02:40:16 -0600

    code sniffer

--- autoloader.php
+++ autoloader.php
@@ -14,75 +14,75 @@ defined( 'ABSPATH' ) || exit;
  */
 class Autoloader {
 
-    /**
-     * Register the autoloader.
-     *
-     * @param string $namespace The plugin namespace.
-     * @param string $dir The directory to autoload from.
-     */
-    public static function register( $namespace, $dir ) {
-        spl_autoload_register( array( __CLASS__, 'autoload' ) );
-        self::add_namespace( $namespace, $dir );
-    }
+/**
+ * Register the autoloader.
+ *
+ * @param string $namespace The plugin namespace.
+ * @param string $dir The directory to autoload from.
+ */
+public static function register( $namespace, $dir ) {
+	spl_autoload_register( array( __CLASS__, 'autoload' ) );
+	self::add_namespace( $namespace, $dir );
+}
 
-    /**
-     * Autoload the class.
-     *
-     * @param string $class_name The class name to autoload.
-     */
-    public static function autoload( $class_name ) {
-        $namespaces = self::get_namespaces();
+/**
+ * Autoload the class.
+ *
+ * @param string $class_name The class name to autoload.
+ */
+public static function autoload( $class_name ) {
+	$namespaces = self::get_namespaces();
 
-        foreach ( $namespaces as $namespace => $dir ) {
-            if ( strpos( $class_name, $namespace . '\\' ) !== 0 ) {
-                continue;
-            }
-            // Remove the 'ChallengePlugin\' namespace prefix and convert class name to file name.
-            $file_name = str_replace( 'ChallengePlugin\\', '', $class_name );
-            $file_name = str_replace( '\\', DIRECTORY_SEPARATOR, $file_name );
-            $file_name = str_replace( '_', '-', $file_name );
-            $file_name = '/class-' . strtolower( $file_name ) . '.php';
+	foreach ( $namespaces as $namespace => $dir ) {
+		if ( strpos( $class_name, $namespace . '\\' ) !== 0 ) {
+			continue;
+		}
+		// Remove the 'ChallengePlugin\' namespace prefix and convert class name to file name.
+		$file_name = str_replace( 'ChallengePlugin\\', '', $class_name );
+			$file_name = str_replace( '\\', DIRECTORY_SEPARATOR, $file_name );
+		$file_name = str_replace( '_', '-', $file_name );
+		$file_name = '/class-' . strtolower( $file_name ) . '.php';
 
-            // Build the full path to the file.
-            $file_path = $dir . $file_name;
+		// Build the full path to the file.
+		$file_path = $dir . $file_name;
 
-            // Include the file if it exists.
-            if ( file_exists( $file_path ) ) {
-                include_once $file_path;
-            }
-        }
-    }
+		// Include the file if it exists.
+		if ( file_exists( $file_path ) ) {
+			include_once $file_path;
+		}
+	}
+}
 
-    /**
-     * Add a namespace to the autoloader.
-     *
-     * @param string $namespace The plugin namespace.
-     * @param string $dir The directory to autoload from.
-     */
-    public static function add_namespace( $namespace, $dir ) {
-        $namespaces = self::get_namespaces();
-        $namespaces[ $namespace ] = $dir;
-        self::set_namespaces( $namespaces );
-    }
+/**
+ * Add a namespace to the autoloader.
+ *
+ * @param string $namespace The plugin namespace.
+ * @param string $dir The directory to autoload from.
+ */
+public static function add_namespace( $namespace, $dir ) {
+	$namespaces = self::get_namespaces();
+	$namespaces[ $namespace ] = $dir;
+	self::set_namespaces( $namespaces );
+}
 
-    /**
-     * Get the list of registered namespaces.
-     *
-     * @return array The list of namespaces.
-     */
-    public static function get_namespaces() {
-        $namespaces = get_option( 'challenge_plugin_namespaces', array() );
-        return $namespaces;
-    }
+/**
+ * Get the list of registered namespaces.
+ *
+ * @return array The list of namespaces.
+ */
+public static function get_namespaces() {
+	$namespaces = get_option( 'challenge_plugin_namespaces', array() );
+	return $namespaces;
+}
 
-    /**
-     * Set the list of registered namespaces.
-     *
-     * @param array $namespaces The list of namespaces.
-     */
-    public static function set_namespaces( $namespaces ) {
-        update_option( 'challenge_plugin_namespaces', $namespaces );
-    }
+/**
+ * Set the list of registered namespaces.
+ *
+ * @param array $namespaces The list of namespaces.
+ */
+public static function set_namespaces( $namespaces ) {
+	update_option( 'challenge_plugin_namespaces', $namespaces );
+}
 }
 
 // Register the Challenge Plugin namespace.
--- includes/class-admin-page.php
+++ includes/class-admin-page.php
@@ -8,55 +8,69 @@
 namespace ChallengePlugin;
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 if ( ! class_exists( 'WP_List_Table' ) ) {
-    require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+	require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
 }
 
+/**
+ * Class for Admin page table
+ */
 class Admin_Page {
-    public function __construct( ) {
-        add_action( 'admin_menu', array( $this, 'register') );
-    }
+	/**
+	 * Add admin menu
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'register' ) );
+	}
 
-    public function render() {
-        wp_enqueue_script( 'challenge-admin-js', CHALLENGE_PLUGIN_URL . 'js/admin.js', array(), CHALLENGE_PLUGIN_VERSION, true );
-        wp_enqueue_style( 'challenge-admin-css', CHALLENGE_PLUGIN_URL . 'css/admin.css', array(), CHALLENGE_PLUGIN_VERSION, 'all' );
-?>
-<div class="frm_wrap">
-<div id="frm_top_bar" class="frm_nav_bar">
-	<a href="#" class="frm-header-logo">
-        <img src="<?php echo CHALLENGE_PLUGIN_URL; ?>images/logo.svg" alt="" width="35px">
-    </a>
-	<div class="frm_top_left">
+	/**
+	 * Render the admin page
+	 *
+	 * @return void
+	 */
+	public function render() {
+		wp_enqueue_script( 'challenge-admin-js', CHALLENGE_PLUGIN_URL . 'js/admin.js', array(), CHALLENGE_PLUGIN_VERSION, true );
+		wp_enqueue_style( 'challenge-admin-css', CHALLENGE_PLUGIN_URL . 'css/admin.css', array(), CHALLENGE_PLUGIN_VERSION, 'all' );
+		?>
+		<div class="frm_wrap">
+		<div id="frm_top_bar" class="frm_nav_bar">
+			<a href="#" class="frm-header-logo">
+				<img src="<?php echo esc_attr( CHALLENGE_PLUGIN_URL ); ?>images/logo.svg" alt="" width="35px">
+			</a>
+		<div class="frm_top_left">
 		<h1>
-        API Table
-        </h1>
+			API Table
+		</h1>
+		</div>
+		<div id="frm-publishing">
+			<button id="refresh-data" class="button frm-button-secondary frm_animate_bg"> Refresh Data </button>
+		</div>
 	</div>
-    <div id="frm-publishing">
-        <button id="refresh-data" class="button frm-button-secondary frm_animate_bg"> Refresh Data </button>
-    </div>        
-</div>
-<hr>
-    <?php
-            // Create an instance of the table
-            $table = new Challenge_List_Table();
-            $table->prepare_items();
-            $table->display();
-        
+	<hr>
+		<?php
+		// Create an instance of the table.
+		$table = new Challenge_List_Table();
+		$table->prepare_items();
+		$table->display();
+		echo '</div></div>';
+	}
 
-        echo '</div></div>';
-    }   
-
-    public function register() {
-        add_menu_page(
-            'Challenge Page',
-            'My Challenge',
-            'manage_options',
-            'challenge-plugin',
-            array( $this, 'render'),
-            'dashicons-chart-area'
-        );
-    }    
+	/**
+	 * Register the WordPress sidebar admin menu
+	 *
+	 * @return void
+	 */
+	public function register() {
+		add_menu_page(
+			'Challenge Page',
+			'My Challenge',
+			'manage_options',
+			'challenge-plugin',
+			array( $this, 'render' ),
+			'dashicons-chart-area'
+		);
+	}
 }
