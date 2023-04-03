f27a95a2d762160de6c3ec5defd731da93fd9b4d
bugsinx <bugsinx@gmail.com>
2023-04-03 02:50:57 -0600

    sniffered

--- includes/class-wp-cli-commands.php
+++ includes/class-wp-cli-commands.php
@@ -10,7 +10,7 @@ namespace ChallengePlugin;
 defined( 'ABSPATH' ) || exit;
 
 if ( ! class_exists( '\WP_CLI' ) ) {
-    return;
+	return;
 }
 
 /**
@@ -18,21 +18,21 @@ if ( ! class_exists( '\WP_CLI' ) ) {
  */
 class WP_CLI_Commands {
 
-    /**
-     * Refresh the challenge plugin data.
-     *
-     * ## EXAMPLES
-     *
-     * wp challenge-plugin refresh-data
-     *
-     * @when after_wp_load
-     */
-    public function refresh_data() {
-        // Implement the refresh logic here.
-        // For example, you could delete any cached data.
-        delete_transient( 'challenge_plugin_data' );
-        \WP_CLI::success( 'Challenge Plugin data has been refreshed.' );
-    }
+	/**
+	 * Refresh the challenge plugin data.
+	 *
+	 * ## EXAMPLES
+	 *
+	 * wp challenge-plugin refresh-data
+	 *
+	 * @when after_wp_load
+	 */
+	public function refresh_data() {
+		// Implement the refresh logic here.
+		// For example, you could delete any cached data.
+		delete_transient( 'challenge_plugin_data' );
+		\WP_CLI::success( 'Challenge Plugin data has been refreshed.' );
+	}
 
 }
 
