b8d32b2b6dc7856e929ac6b0e3167abcef102624
bugsinx <bugsinx@gmail.com>
2023-04-03 02:45:18 -0600

    Code Sniifered

--- includes/class-ajax-handler.php
+++ includes/class-ajax-handler.php
@@ -14,90 +14,102 @@ defined( 'ABSPATH' ) || exit;
  */
 class Ajax_Handler {
 
-    /**
-     * Initialize the AJAX handler.
-     */
-    public function __construct( $init = true ) {
-        if ( $init ) {
-            add_action( 'wp_ajax_challenge_plugin_data', array( $this, 'get_data' ) );
-            add_action( 'wp_ajax_nopriv_challenge_plugin_data', array( $this, 'get_data' ) );
-            add_action( 'wp_ajax_challenge_plugin_refresh_data', array( $this, 'refresh_data' ) );
-        }
-    }
+	/**
+	 * Add all the ajax endpoints to WordPress
+	 *
+	 * @param boolean $init Register or not the ajax end points.
+	 */
+	public function __construct( $init = true ) {
+		if ( $init ) {
+			add_action( 'wp_ajax_challenge_plugin_data', array( $this, 'get_data' ) );
+			add_action( 'wp_ajax_nopriv_challenge_plugin_data', array( $this, 'get_data' ) );
+			add_action( 'wp_ajax_challenge_plugin_refresh_data', array( $this, 'refresh_data' ) );
+		}
+	}
 
-    /**
-     * Get the data from the API endpoint.
-     */
-    public function get_data( $no_ajax = false ) {
-        $data = self::get_cached_data();
-        
-        if ( ! empty( $data ) && true !== $no_ajax ) {
-            wp_send_json_success( $data );
-        }
-        if ( ! empty( $data ) && true === $no_ajax ) {
-            return $data;
-        }        
+	/**
+	 * Return the data from the API or cache
+	 *
+	 * @param boolean $no_ajax // Avoid returning a json respond and just return the array.
+	 * @return array
+	 */
+	public function get_data( $no_ajax = false ) {
+		$data = self::get_cached_data();
 
-        $response = wp_remote_get( 'http://api.strategy11.com/wp-json/challenge/v1/1' );
-        if ( is_wp_error( $response )  && true !== $no_ajax ) {
-            wp_send_json_error( array(
-                'error' => $response->get_error_message(),
-            ) );
-        }
+		if ( ! empty( $data ) && true !== $no_ajax ) {
+			wp_send_json_success( $data );
+		}
+		if ( ! empty( $data ) && true === $no_ajax ) {
+			return $data;
+		}
 
-        $body = wp_remote_retrieve_body( $response );
-        $data = json_decode( $body );
+		$response = wp_remote_get( 'http://api.strategy11.com/wp-json/challenge/v1/1' );
+		if ( is_wp_error( $response ) && true !== $no_ajax ) {
+			wp_send_json_error(
+				array(
+					'error' => $response->get_error_message(),
+				)
+			);
+		}
 
-        if ( empty( $data )  && true !== $no_ajax ) {
-            wp_send_json_error( array(
-                'error' => __( 'Error retrieving data from API endpoint.', 'challenge-plugin' ),
-            ) );
-        }
+		$body = wp_remote_retrieve_body( $response );
+		$data = json_decode( $body );
 
-        self::cache_data( $data );
-        if ( true !== $no_ajax ) {
-            wp_send_json_success( $data );
-        } else {
-            return $data;
-        }
-    }
+		if ( empty( $data ) && true !== $no_ajax ) {
+			wp_send_json_error(
+				array(
+					'error' => __( 'Error retrieving data from API endpoint.', 'challenge-plugin' ),
+				)
+			);
+		}
 
-    /**
-     * Refresh the cached data.
-     */
-    public function refresh_data() {        
-        self::cache_data( null );
+		self::cache_data( $data );
+		if ( true !== $no_ajax ) {
+			wp_send_json_success( $data );
+		} else {
+			return $data;
+		}
+	}
 
-        if ( isset($_POST['ajax']) ) {
-            wp_send_json_success( 1 );
-        }
-    }
+	/**
+	 * Refresh the cached data.
+	 */
+	public function refresh_data() {
+		self::cache_data( null );
 
-    /**
-     * Get the cached data.
-     *
-     * @return mixed The cached data, or null if there is no cached data.
-     */
-    private function get_cached_data() {
-        $data = get_transient( 'challenge_plugin_data' );
-        if ( empty( $data ) ) {
-            return null;
-        }
+		// phpcs:disable
+		// No need of nonce for this case.
+		if ( isset( $_POST['ajax'] ) ) {
+			wp_send_json_success( 1 );
+		}
+		// phpcs:enable
+	}
 
-        return $data;
-    }
+	/**
+	 * Get the cached data.
+	 *
+	 * @return mixed The cached data, or null if there is no cached data.
+	 */
+	private function get_cached_data() {
+		$data = get_transient( 'challenge_plugin_data' );
+		if ( empty( $data ) ) {
+			return null;
+		}
 
-    /**
-     * Cache the data for 1 hour.
-     *
-     * @param mixed $data The data to cache, or null to clear the cache.
-     */
-    private function cache_data( $data ) {
-        if ( empty( $data ) ) {
-            delete_transient( 'challenge_plugin_data' );
-            return;
-        }
+		return $data;
+	}
 
-        set_transient( 'challenge_plugin_data', $data, HOUR_IN_SECONDS );
-    }
+	/**
+	 * Cache the data for 1 hour.
+	 *
+	 * @param mixed $data The data to cache, or null to clear the cache.
+	 */
+	private function cache_data( $data ) {
+		if ( empty( $data ) ) {
+			delete_transient( 'challenge_plugin_data' );
+			return;
+		}
+
+		set_transient( 'challenge_plugin_data', $data, HOUR_IN_SECONDS );
+	}
 }
