f37b9f5e649d8cbeb1310090c66be025075bfa52
bugsinx <bugsinx@gmail.com>
2023-04-03 02:50:20 -0600

    Code SNiffered

--- includes/class-shortcode.php
+++ includes/class-shortcode.php
@@ -14,36 +14,39 @@ defined( 'ABSPATH' ) || exit;
  */
 class Shortcode {
 
-    public function __construct()
-    {
-        add_shortcode( 'challenge_plugin', array( $this, 'render_shortcode') );
+	/**
+	 * Register the shortcode tag [challenge_plugin]
+	 */
+	public function __construct()
+	{
+		add_shortcode( 'challenge_plugin', array( $this, 'render_shortcode') );
 
-    }
-    /**
-     * Render the shortcode.
-     *
-     * @param array $atts Shortcode attributes.
-     * @return string Shortcode output.
-     */
-    public function render_shortcode( $atts ) {
-        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-        ob_start();
-        ?>
-        <table id="challenge-plugin-table">
-            <tbody></tbody>
-        </table>
-        <?php
-        return ob_get_clean();
-    }
+	}
+	/**
+	 * Render the shortcode.
+	 *
+	 * @param array $atts Shortcode attributes.
+	 * @return string Shortcode output.
+	 */
+	public function render_shortcode( $atts ) {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		ob_start();
+		?>
+		<table id="challenge-plugin-table">
+			<tbody></tbody>
+		</table>
+		<?php
+		return ob_get_clean();
+	}
 
-    /**
-     * Enqueue the necessary scripts.
-     */
-    public function enqueue_scripts() {
-        wp_enqueue_script( 'challenge-plugin-script', CHALLENGE_PLUGIN_URL . 'js/challenge-plugin-script.js', array( 'jquery' ), CHALLENGE_PLUGIN_VERSION, true );
-        wp_localize_script( 'challenge-plugin-script', 'challenge_plugin_data', array(
-            'ajax_url' => admin_url( 'admin-ajax.php' ),
-            'nonce'    => wp_create_nonce( 'challenge_plugin_data_nonce' ),
-        ) );
-    }
+	/**
+	 * Enqueue the necessary scripts.
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'challenge-plugin-script', CHALLENGE_PLUGIN_URL . 'js/challenge-plugin-script.js', array( 'jquery' ), CHALLENGE_PLUGIN_VERSION, true );
+		wp_localize_script( 'challenge-plugin-script', 'challenge_plugin_data', array(
+			'ajax_url' => admin_url( 'admin-ajax.php' ),
+			'nonce'    => wp_create_nonce( 'challenge_plugin_data_nonce' ),
+		) );
+	}
 }
